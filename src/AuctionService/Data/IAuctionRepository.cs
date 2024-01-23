0564b6e1fedc2a33ce9e442e201bf721fae8a3da
Ever <everomar.portillo@gmail.com>
2024-01-23 14:09:39 -0800

    End of Appendix A

---
+++ src/AuctionService/Data/IAuctionRepository.cs
@@ -0,0 +1,16 @@
+﻿using AuctionService.DTOs;
+using AuctionService.Entities;
+
+namespace AuctionService;
+
+//Mocking for Unit Testing
+
+public interface IAuctionRepository
+{
+    Task<List<AuctionDto>> GetAuctionsAsync(string date);
+    Task<AuctionDto> GetAuctionByIdAsync(Guid Id);
+    Task<Auction> GetAuctionEntityById(Guid Id);
+    void AddAuction(Auction auction);
+    void RemoveAuction(Auction auction);
+    Task <bool> SaveChangesAsync();
+}
---
+++ tests/AuctionService.UnitTests/AuctionEntityTests.cs
@@ -0,0 +1,39 @@
+using System.Data.Common;
+using AuctionService.Entities;
+
+namespace AuctionService.UnitTests;
+
+public class AuctionEntityTests
+{
+    //Naming convention for tests
+    //public void Method_Scenario_ExpectedResult()
+    //{
+    //   
+    //}
+    
+    [Fact]
+    public void HasReservePrice_ReservePriceGtZero_True()
+    {
+        //arrange
+        var auction = new Auction{Id = Guid.NewGuid(), ReservePrice = 10};
+
+        //act
+        var result = auction.HasReservePrice();
+
+        //assert
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void HasReservePrice_ReservePriceIsZero_False()
+    {
+        //arrange
+        var auction = new Auction{Id = Guid.NewGuid(), ReservePrice = 0};
+
+        //act
+        var result = auction.HasReservePrice();
+
+        //assert
+        Assert.False(result);
+    }
+}
