4719e64a7c016ed8a1cf371348304bc684f0a59c
Ever <everomar.portillo@gmail.com>
2024-02-11 20:39:13 -0800

    End of Section 10

--- frontend/web-app/app/auctions/AuctionCard.tsx
+++ frontend/web-app/app/auctions/AuctionCard.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import CountdownTimer from './CountdownTimer'
-import Carimage from './Carimage'
 import { Auction } from '@/types'
+import Link from 'next/link'
+import CarImage from './CarImage';
 
 type Props = {
     auction: Auction
@@ -9,10 +10,10 @@ type Props = {
 
 export default function AuctionCard({auction}: Props) {
   return (
-    <a href='#' className='group'>
+    <Link href={`/auctions/details/${auction.id}`} className='group'>
         <div className='w-full bg-gray-200 aspect-w-16 aspect-h-10 rounded-lg overflow-hidden'>
             <div>
-                <Carimage imageUrl={auction.imageUrl} />
+                <CarImage imageUrl={auction.imageUrl} />
                 <div className='absolute bottom-2 left-2'>
                     <CountdownTimer auctionEnd={auction.auctionEnd}/>
                 </div>
@@ -22,6 +23,6 @@ export default function AuctionCard({auction}: Props) {
             <h3 className='text-gray-700'>{auction.make} {auction.model}</h3>
             <p className='font-semibold text-sm'>{auction.year}</p>
         </div>
-    </a>
+    </Link>
   )
 }
